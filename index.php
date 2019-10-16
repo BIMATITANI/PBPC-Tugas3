d9f12cf889d912e4dc06540e9fb6dfa555f7e084
BIMATITANI <bimatitani88@gmail.com>
2019-10-17 02:06:27 +0700

    nmbh nmbah warna dan background

--- index.php
+++ index.php
@@ -7,9 +7,13 @@
 		<title>Hello, world!</title>
 	</head>
 	<body>
+	
 	<style>
 		html, body {
-			background-color: red;
+			background-color: white;
+			background-image: url("1_CikVjfoDbQG_xP24g72-3w.jpeg");
+			color: white;
+			text-align: center;
 		}
 
 		.animasi {
@@ -27,7 +31,11 @@
 				opacity: 1;
 			}
 		}
+		
+		
+		
 	</style>
+
 <?php
 
 session_start();
@@ -36,6 +44,7 @@ if (!isset($_SESSION['jeneng'])) {
 	header("location:login.php");
 	exit;
 }
+
 
 echo '
 		<nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
@@ -60,9 +69,17 @@ echo '
 			</div>
 		</nav>
 		<div class="container-fluid">
-			<h2 class="animasi">Welcome '.$_SESSION['jeneng'].'</h2>
+			<h2 class="animasi" >Welcome '.$_SESSION['jeneng'].'</h2>
 		</div>
+		
+		
+		
+		
+
 ';
+
+
+
 ?>
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
